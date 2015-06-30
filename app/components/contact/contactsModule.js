195445880318ad51e3b3648beb0b3dd1385cd5ac
ivanaszuber <ivanas.zuber@gmail.com>
2015-06-30 08:45:06 +0200

    added contactsModule

---
+++ app/components/contact/contactsModule.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ivana on 30.06.15..
+ */
+define([
+    'angular',
+    'angular-couch-potato',
+    'angular-ui-router',
+    'lodash'
+], function (ng, couchPotato, _) {
+
+    "use strict";
+
+    var contactsModule = ng.module('contactsModule', ['ui.router']);
+
+    couchPotato.configureApp(contactsModule);
+
+    contactsModule.config(function ($stateProvider, $couchPotatoProvider) {
+
+        $stateProvider
+            .state('app.contact', {
+                url: '/contact',
+                views: {
+                    content: {
+                        templateUrl: 'components/contact/contactsView.html',
+                        controller: 'contactsController',
+                        resolve: {
+                            deps: $couchPotatoProvider.resolveDependencies([])
+                        }
+                    }
+                }
+            })
+    });
+
+    contactsModule.run(function ($couchPotato) {
+        contactsModule.lazy = $couchPotato;
+    });
+
+    return contactsModule;
+
+});
