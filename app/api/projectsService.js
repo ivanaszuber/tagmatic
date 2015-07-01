dadf03a372f5f4d7c00141f62cdedc8278f1bd94
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-01 14:51:51 +0200

    projects API service

---
+++ app/api/projectsService.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ivana on 01.07.15..
+ */
+
+define(['appModule'], function (module) {
+
+    'use strict';
+
+    return module.registerFactory('projectsService', function (apiService) {
+
+        return {
+
+            'getProjectList': function () {
+                return apiService.request({
+                    'method': 'GET',
+                    'url': '/projects'
+                })
+            },
+
+            'createProject': function (project) {
+                return apiService.request({
+                    'method': 'POST',
+                    'url': '/projects',
+                    'data': project
+                })
+            }
+        }
+    })
+})
+;
