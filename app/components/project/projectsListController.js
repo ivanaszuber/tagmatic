d1f80cb1b240da939126d29525ded02bb0cae44a
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-01 19:33:51 +0200

    added project grid buttons

--- app/components/project/projectsListController.js
+++ app/components/project/projectsListController.js
@@ -16,30 +16,30 @@ define(['appModule'], function (module) {
                 {field: 'name', displayName: 'Name'},
                 {field: 'description', displayName: 'Description'},
                 {field: 'getFirstAndLastName()', displayName: 'Project Lead'},
-                //{
-                //    field: 'id',
-                //    displayName: '',
-                //    cellTemplate: 'editButton.html',
-                //    width: 40,
-                //    enableColumnMenu: false,
-                //    enableSorting: false
-                //},
-                //{
-                //    field: 'id',
-                //    displayName: '',
-                //    cellTemplate: 'viewButton.html',
-                //    width: 40,
-                //    enableColumnMenu: false,
-                //    enableSorting: false
-                //},
-                //{
-                //    field: 'id',
-                //    displayName: '',
-                //    cellTemplate: 'deleteButton.html',
-                //    width: 40,
-                //    enableColumnMenu: false,
-                //    enableSorting: false
-                //}
+                {
+                    field: 'id',
+                    displayName: '',
+                    cellTemplate: 'editButton.html',
+                    width: 40,
+                    enableColumnMenu: false,
+                    enableSorting: false
+                },
+                {
+                    field: 'id',
+                    displayName: '',
+                    cellTemplate: 'viewButton.html',
+                    width: 40,
+                    enableColumnMenu: false,
+                    enableSorting: false
+                },
+                {
+                    field: 'id',
+                    displayName: '',
+                    cellTemplate: 'deleteButton.html',
+                    width: 40,
+                    enableColumnMenu: false,
+                    enableSorting: false
+                }
             ],
             data: 'projects',
             multiSelect: true,
