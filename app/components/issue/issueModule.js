1d8cbaf21d96326cd97dbd1f770c291abbbc0c08
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-02 09:14:01 +0200

    issueModule.js

---
+++ app/components/issue/issueModule.js
@@ -0,0 +1,46 @@
+/**
+ * Created by ivana on 02.07.15..
+ */
+define([
+    'angular',
+    'angular-couch-potato',
+    'angular-ui-router',
+    'lodash'
+], function (ng, couchPotato, _) {
+
+    "use strict";
+
+    var issueModule = ng.module('issueModule', [
+        'ui.router',
+        'ui.grid',
+        'ui.grid.selection',
+        'ui.grid.autoResize',
+        'ui.bootstrap'
+    ]);
+
+    couchPotato.configureApp(issueModule);
+
+    issueModule.config(function ($stateProvider, $couchPotatoProvider) {
+
+        $stateProvider
+            .state('app.issue', {
+                url: '/issue',
+                views: {
+                    content: {
+                        templateUrl: 'components/issue/issueListView.html',
+                        controller: 'issuesListController',
+                        resolve: {
+                            deps: $couchPotatoProvider.resolveDependencies([])
+                        }
+                    }
+                }
+            })
+    });
+
+    issueModule.run(function ($couchPotato) {
+        issueModule.lazy = $couchPotato;
+    });
+
+    return issueModule;
+
+});
