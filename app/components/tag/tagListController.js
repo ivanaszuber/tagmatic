635f2492375c1de99a78129000ac558b1a46e283
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-02 14:40:42 +0200

    tagListController and methods

---
+++ app/components/tag/tagListController.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ivana on 02.07.15..
+ */
+
+define(['appModule'], function (module) {
+
+    "use strict";
+
+    module.registerController('tagListController', function ($scope, $state, tagService) {
+
+        $scope.tags = [];
+
+        tagService.getTagList()
+            .then(function (tags) {
+                $scope.tags = tags
+            });
+
+
+        $scope.createTag = function () {
+            tagService.createTag($scope.tag).then(function () {
+                $scope.tag = '';
+            });
+        };
+
+        $scope.delete = function () {
+            tagService.deleteTag().then(function (data) {
+                $scope.tags = data;
+            })
+        }
+
+    })
+});
