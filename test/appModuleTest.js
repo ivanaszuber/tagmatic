6f22e2f7b0ad8b57dece799811cb69ad78c187ad
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-04 22:18:38 +0200

    added appModule unit test (loading test dependencies)

---
+++ test/appModuleTest.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Ivana on 13.6.2015..
+ */
+define([
+    'angular',
+    'angular-mocks',
+    'loginController',
+    'loginModule',
+    'layoutModule',
+    'todoModule',
+    'contactsModule',
+    'homeModule',
+    'projectModule',
+    'issueModule',
+    'tagModule',
+    'administrationModule',
+    'authService'
+], function (appModule) {
+
+    describe('appModule', function () {
+
+        beforeEach(module('scs.couch-potato'));
+
+    })
+})
