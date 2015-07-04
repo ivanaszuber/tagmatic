48e0ec6ed17242356c2c67fb40db66fb826db145
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-04 22:55:37 +0200

    fixed login unit tests

--- test/components/login/loginControllerTest.js
+++ test/components/login/loginControllerTest.js
@@ -4,7 +4,7 @@
 
 define([
     'loginController',
-    'appModule'
+    'authService'
 ], function (loginController) {
 
     describe('loginController', function () {
@@ -18,19 +18,17 @@ define([
         beforeEach(module('appModule'));
 
 
-        var scope, $httpBackend, AuthService, $timeout, state, $q;
+        var scope, $httpBackend, AuthService, $timeout, state, $q, deferred;
 
         /**
          * Injecting the dependencies
          */
-        beforeEach(inject(function ($controller, authService, $state) {
-            scope = {};
-            AuthService = {};
+        beforeEach(inject(function ($controller, authService, $state, $rootScope) {
+            scope = $rootScope.$new();
             state = $state;
+            AuthService = authService;
             ctrl = $controller('loginController', {
-                $scope: scope,
-                $state: state,
-                AuthService: authService
+                $scope: scope
             });
         }));
 
@@ -50,21 +48,39 @@ define([
         }));
 
         beforeEach(function () {
+            deferred = $q.defer();
+            spyOn(scope, 'login').and.callThrough();
+            spyOn(AuthService, 'login').and.returnValue(deferred.promise);
+
             scope.model = {
                 'email': 'test@test.com',
                 'password': 'test'
             };
             spyOn(state, 'go');
+            spyOn(deferred.promise, 'then').and.callThrough();
+
+
+        });
+
+        beforeEach(function () {
             scope.login(scope.model.email, scope.model.password);
+        });
+
+        beforeEach(function () {
+            deferred.resolve(scope.model);
+            scope.$digest();
         });
 
         it('should have a model defined', function () {
             expect(scope.model).toBeDefined();
         });
 
-        it('should successfully log in the user', function () {
-            $httpBackend.expectPOST('http://localhost:5005/api/v1/sessions');
-            $httpBackend.flush();
+        it('should call AuthService.login', function () {
+            expect(AuthService.login).toHaveBeenCalled();
+        });
+
+        it('should set the state to app.home', function () {
+            expect(state.go).toHaveBeenCalledWith('app.home');
         });
     });
 });
