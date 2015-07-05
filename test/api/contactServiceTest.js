4ee5bf2230be462af8703b83015040f70f460efc
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-05 09:15:10 +0200

    contactService create, edit and delete contact unit tests

--- test/api/contactServiceTest.js
+++ test/api/contactServiceTest.js
@@ -109,6 +109,158 @@ define([
             })
         });
 
+        describe('createContact', function () {
+            var contact = {
+                "name": "test",
+                "description": "test"
+            };
+
+            beforeEach(inject(function ($injector) {
+                $httpBackend = $injector.get('$httpBackend');
+                $httpBackend.when('POST', 'http://localhost:5005/api/v1/contacts')
+                    .respond(200, {
+                        "name": "test",
+                        "description": "test"
+                    });
+                $httpBackend.when('GET', 'components/layout/layoutView.html')
+                    .respond(200);
+                $httpBackend.when('GET', 'components/todo/todoView.html')
+                    .respond(200);
+            }));
+
+            it('should send a POST request to api/v1/contacts', function () {
+                ContactService.createContact(contact);
+                $httpBackend.expectPOST('http://localhost:5005/api/v1/contacts');
+                $httpBackend.flush();
+            });
+
+
+            describe('createContact request chain', function () {
+                beforeEach(function () {
+                    deferred = q.defer();
+
+                    spyOn(ContactService, 'createContact').and.callThrough();
+                    spyOn(ApiService, 'request').and.returnValue(deferred.promise);
+                });
+
+
+                beforeEach(function () {
+                    ContactService.createContact(contact);
+                });
+
+
+                it('should call apiService.request', function () {
+                    expect(ApiService.request).toHaveBeenCalled();
+                    deferred.resolve(contact);
+                    $scope.$apply();
+                });
+
+            })
+
+
+        });
+
+        describe('editContact', function () {
+            var contact = {
+                "name": "test",
+                "description": "test"
+            };
+
+            beforeEach(inject(function ($injector) {
+                $httpBackend = $injector.get('$httpBackend');
+                $httpBackend.when('PUT', 'http://localhost:5005/api/v1/contacts/1')
+                    .respond(200, {
+                        "name": "test",
+                        "description": "test"
+                    });
+                $httpBackend.when('GET', 'components/layout/layoutView.html')
+                    .respond(200);
+                $httpBackend.when('GET', 'components/todo/todoView.html')
+                    .respond(200);
+            }));
+
+            it('should send a PUT request to api/v1/contacts', function () {
+                ContactService.editContact(id, contact);
+                $httpBackend.expectPUT('http://localhost:5005/api/v1/contacts/1');
+                $httpBackend.flush();
+            });
+
+
+            describe('editContact request chain', function () {
+                beforeEach(function () {
+                    deferred = q.defer();
+
+                    spyOn(ContactService, 'editContact').and.callThrough();
+                    spyOn(ApiService, 'request').and.returnValue(deferred.promise);
+                });
+
+
+                beforeEach(function () {
+                    ContactService.editContact(id, contact);
+                });
+
+
+                it('should call apiService.request', function () {
+                    expect(ApiService.request).toHaveBeenCalled();
+                    deferred.resolve(contact);
+                    $scope.$apply();
+                });
+
+            })
+
+        });
+
+        describe('deleteContact', function () {
+            var contact = {
+                "name": "test",
+                "description": "test"
+            };
+
+            beforeEach(inject(function ($injector) {
+                $httpBackend = $injector.get('$httpBackend');
+                $httpBackend.when('DELETE', 'http://localhost:5005/api/v1/contacts/1')
+                    .respond(200, {
+                        "name": "test",
+                        "description": "test"
+                    });
+                $httpBackend.when('GET', 'components/layout/layoutView.html')
+                    .respond(200);
+                $httpBackend.when('GET', 'components/todo/todoView.html')
+                    .respond(200);
+            }));
+
+            it('should send a DELETE request to api/v1/contacts', function () {
+                ContactService.deleteContact(id);
+                $httpBackend.expectDELETE('http://localhost:5005/api/v1/contacts/1');
+                $httpBackend.flush();
+            });
+
+
+            describe('deleteContact request chain', function () {
+                beforeEach(function () {
+                    deferred = q.defer();
+
+                    spyOn(ContactService, 'deleteContact').and.callThrough();
+                    spyOn(ApiService, 'request').and.returnValue(deferred.promise);
+                });
+
+
+                beforeEach(function () {
+                    ContactService.deleteContact(id);
+                });
+
+
+                it('should call apiService.request', function () {
+                    expect(ApiService.request).toHaveBeenCalled();
+                    deferred.resolve(contact);
+                    $scope.$apply();
+                });
+
+            })
+
+
+        });
+
     });
 
 });
