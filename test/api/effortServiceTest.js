57c2fa04bd98ebd8bbec2938181fc73334c802ef
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-05 09:16:01 +0200

    added effort service crud unit tests

--- test/api/effortServiceTest.js
+++ test/api/effortServiceTest.js
@@ -107,6 +107,157 @@ define([
             })
         });
 
+        describe('createEffort', function () {
+            var effort = {
+                "name": "test",
+                "description": "test"
+            };
+
+            beforeEach(inject(function ($injector) {
+                $httpBackend = $injector.get('$httpBackend');
+                $httpBackend.when('POST', 'http://localhost:5005/api/v1/efforts')
+                    .respond(200, {
+                        "name": "test",
+                        "description": "test"
+                    });
+                $httpBackend.when('GET', 'components/layout/layoutView.html')
+                    .respond(200);
+                $httpBackend.when('GET', 'components/todo/todoView.html')
+                    .respond(200);
+            }));
+
+            it('should send a POST request to api/v1/efforts', function () {
+                EffortService.createEffort(effort);
+                $httpBackend.expectPOST('http://localhost:5005/api/v1/efforts');
+                $httpBackend.flush();
+            });
+
+
+            describe('createEffort request chain', function () {
+                beforeEach(function () {
+                    deferred = q.defer();
+
+                    spyOn(EffortService, 'createEffort').and.callThrough();
+                    spyOn(ApiService, 'request').and.returnValue(deferred.promise);
+                });
+
+
+                beforeEach(function () {
+                    EffortService.createEffort(effort);
+                });
+
+
+                it('should call apiService.request', function () {
+                    expect(ApiService.request).toHaveBeenCalled();
+                    deferred.resolve(effort);
+                    $scope.$apply();
+                });
+
+            })
+
+
+        });
+
+        describe('editEffort', function () {
+            var effort = {
+                "name": "test",
+                "description": "test"
+            };
+
+            beforeEach(inject(function ($injector) {
+                $httpBackend = $injector.get('$httpBackend');
+                $httpBackend.when('PUT', 'http://localhost:5005/api/v1/efforts/1')
+                    .respond(200, {
+                        "name": "test",
+                        "description": "test"
+                    });
+                $httpBackend.when('GET', 'components/layout/layoutView.html')
+                    .respond(200);
+                $httpBackend.when('GET', 'components/todo/todoView.html')
+                    .respond(200);
+            }));
+
+            it('should send a PUT request to api/v1/efforts', function () {
+                EffortService.editEffort(id, effort);
+                $httpBackend.expectPUT('http://localhost:5005/api/v1/efforts/1');
+                $httpBackend.flush();
+            });
+
+
+            describe('editEffort request chain', function () {
+                beforeEach(function () {
+                    deferred = q.defer();
+
+                    spyOn(EffortService, 'editEffort').and.callThrough();
+                    spyOn(ApiService, 'request').and.returnValue(deferred.promise);
+                });
+
+
+                beforeEach(function () {
+                    EffortService.editEffort(id, effort);
+                });
+
+
+                it('should call apiService.request', function () {
+                    expect(ApiService.request).toHaveBeenCalled();
+                    deferred.resolve(effort);
+                    $scope.$apply();
+                });
+
+            })
+
+        });
+
+        describe('deleteEffort', function () {
+            var effort = {
+                "name": "test",
+                "description": "test"
+            };
+
+            beforeEach(inject(function ($injector) {
+                $httpBackend = $injector.get('$httpBackend');
+                $httpBackend.when('DELETE', 'http://localhost:5005/api/v1/efforts/1')
+                    .respond(200, {
+                        "name": "test",
+                        "description": "test"
+                    });
+                $httpBackend.when('GET', 'components/layout/layoutView.html')
+                    .respond(200);
+                $httpBackend.when('GET', 'components/todo/todoView.html')
+                    .respond(200);
+            }));
+
+            it('should send a DELETE request to api/v1/efforts', function () {
+                EffortService.deleteEffort(id);
+                $httpBackend.expectDELETE('http://localhost:5005/api/v1/efforts/1');
+                $httpBackend.flush();
+            });
+
+
+            describe('deleteEffort request chain', function () {
+                beforeEach(function () {
+                    deferred = q.defer();
+
+                    spyOn(EffortService, 'deleteEffort').and.callThrough();
+                    spyOn(ApiService, 'request').and.returnValue(deferred.promise);
+                });
+
+
+                beforeEach(function () {
+                    EffortService.deleteEffort(id);
+                });
+
+
+                it('should call apiService.request', function () {
+                    expect(ApiService.request).toHaveBeenCalled();
+                    deferred.resolve(effort);
+                    $scope.$apply();
+                });
+
+            })
+
+
+        });
     });
 
 });
