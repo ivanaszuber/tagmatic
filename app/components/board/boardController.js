edaabc959ef0013f5e968de5aaf12a6bb5faf7a3
ivanaszuber <ivanas.zuber@gmail.com>
2015-07-05 12:11:20 +0200

    added create column button and method

--- app/components/board/boardController.js
+++ app/components/board/boardController.js
@@ -6,18 +6,45 @@ define(['appModule'], function (module) {
 
     "use strict";
 
-    module.registerController('boardController', function ($scope, columnService) {
+    module.registerController('boardController', function ($scope, columnService, $modal, $rootScope) {
 
         $scope.columns = [];
         $scope.isLoading = false;
 
-        $scope.refreshBoard = function refreshBoard() {
+        $rootScope.refreshBoard = function refreshBoard() {
             $scope.isLoading = true;
             columnService.getColumnList()
                 .then(function (data) {
                     $scope.isLoading = false;
                     $scope.columns = data;
                 });
+        };
+
+        $scope.createColumn = function () {
+            $modal.open({
+                templateUrl: 'components/administration/column/columnCreateView.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.column = {};
+                    $scope.submitted = false;
+
+                    $scope.newColumn = function (isValid) {
+
+                        $scope.submitted = true;
+
+                        if (isValid) {
+                            columnService.createColumn($scope.column).then(function () {
+                                $modalInstance.close();
+                                $rootScope.refreshBoard();
+                            });
+                        }
+
+                    };
+
+                    $scope.closeModal = function () {
+                        $modalInstance.close();
+                    }
+                }
+            });
         };
 
         $scope.refreshBoard();
